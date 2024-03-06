97045c8454b3094342840fed2c5a22a08f822c1b
gabrielprns <64814557+gabrielprns@users.noreply.github.com>
2024-03-06 17:54:57 -0300

    Project

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,7 @@ import { logo, menu, close} from "../assets";
 
 
 const Navbar = () => {
+  const[active, setActive] = useState("")
   return (
     <nav className={`${styles.paddingX} w-full flex 
     item-center py-5 fixed top-0 z-30 bg-primary`}
@@ -16,6 +17,34 @@ const Navbar = () => {
       <div className="w-full flex justify-between 
       items-center max-w-7x1 mx-auto">
 
+        <Link
+          to="/"
+          className="flex items-center gap-2"
+          onClick={()=>{
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img src={logo} alt="logo" 
+          className="w-9 h-9 object-contain"/>
+
+          <p className="text-white text-[18px] font-bold 
+          cursor-pointer">Gabriel <span
+          className="sm:block hidden">|Web Developer      
+          </span></p>
+        </Link>
+        
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((Link)=>{
+            
+              <li>
+                <a href={`#${Link.id}`}>{Link.title}</a>
+              </li>
+          })        
+          }
+
+          </ul>
+      
       </div>
 
     </nav>
